0f235cd82591bd102dcff334102f78a63967ba81
Blarissa <larissabrasil009@gmail.com>
2024-11-18 16:53:25 -0300

    feat: adicionando componente 'Capa' e atualizando estrutura do cabeçalho

--- dorothy-piaui/src/app/app.component.ts
+++ dorothy-piaui/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
+import { CapaComponent } from "./capa/capa.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, WelcomeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, WelcomeComponent, CapaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
