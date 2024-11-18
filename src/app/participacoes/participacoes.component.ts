a74cc88d35db2563961c6796c167a9d5a776b865
Blarissa <larissabrasil009@gmail.com>
2024-11-18 19:00:44 -0300

    config: ajustes nas configurações

---
+++ src/app/participacoes/participacoes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-participacoes',
+  standalone: true,
+  imports: [],
+  templateUrl: './participacoes.component.html',
+  styleUrl: './participacoes.component.css'
+})
+export class ParticipacoesComponent {
+
+}
