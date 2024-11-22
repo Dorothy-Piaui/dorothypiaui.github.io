aa5250510d053c07d81b83bd49f012bc6318601b
Blarissa <larissabrasil009@gmail.com>
2024-11-22 18:02:17 -0300

    refactor: atualizar layout e estilos para cabeçalho, rodapé e novos componentes de participação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
-import { CapaComponent } from "./capa/capa.component";
 import { RealizacoesComponent } from "./realizacoes/realizacoes.component";
 import { ParticipacoesComponent } from './participacoes/participacoes.component';
+import { CapaComponent } from "./capa/capa.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    HeaderComponent, 
-    FooterComponent, 
-    WelcomeComponent, 
-    CapaComponent,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeComponent,
     RealizacoesComponent,
-    ParticipacoesComponent
-  ],
+    ParticipacoesComponent,
+    CapaComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/participacoes/participacoes.component.ts
+++ src/app/participacoes/participacoes.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-participacoes',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './participacoes.component.html',
   styleUrl: './participacoes.component.css'
 })
